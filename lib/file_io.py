28f28baccb82f6d66083945642b4b111bc0e65dc
FlozyTess <cecilrency23@gmail.com>
2025-03-17 08:35:13 +0300

    appended file.io

--- lib/file_io.py
+++ lib/file_io.py
@@ -19,11 +19,26 @@ def append_file(file_name, append_content):
     :param file_name: Name of the file without extension.
     :param append_content: Content to append to the file.
     """
-    full_path = f"{file_name}.txt"
-
+    full_path = os.path.join(str(file_name) + ".txt") if isinstance(file_name, str) else str(file_name) + ".txt"
+    
     with open(full_path, "a") as file:
-            
+        if os.path.getsize(full_path) > 0 and not append_content.startswith("\n"):
+            file.write("\n") 
+        file.write(append_content) 
     pass
 
 def read_file(file_name):
+    """
+    Reads and returns the content of a .txt file.
+    
+    :param file_name: Name of the file without extension.
+    :return: Content of the file as a string.
+    """
+    full_path = os.path.join(str(file_name) + ".txt") if isinstance(file_name, str) else str(file_name) + ".txt"
+    
+    if not os.path.exists(full_path):
+        return "File not found."
+
+    with open(full_path, "r") as file:
+        return file.read().strip() 
     pass
