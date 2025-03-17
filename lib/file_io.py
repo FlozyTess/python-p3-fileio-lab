86677ac2b9b91db42152e09d047506bf1432a541
FlozyTess <cecilrency23@gmail.com>
2025-03-17 08:20:27 +0300

    wrote file

--- lib/file_io.py
+++ lib/file_io.py
@@ -1,7 +1,28 @@
+import os
+
 def write_file(file_name, file_content):
+    """
+    Creates a new .txt file or overwrites an existing file with the given content.
+    
+    :param file_name: Name of the file without extension.
+    :param file_content: Content to write in the file.
+    """
+    full_path = f"{file_name}.txt"
+    with open(full_path, "w") as file:
+            file.write(file_content)
     pass
 
 def append_file(file_name, append_content):
+    """
+    Appends content to an existing .txt file.
+    
+    :param file_name: Name of the file without extension.
+    :param append_content: Content to append to the file.
+    """
+    full_path = f"{file_name}.txt"
+
+    with open(full_path, "a") as file:
+            
     pass
 
 def read_file(file_name):
